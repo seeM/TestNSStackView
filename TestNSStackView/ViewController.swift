cfc797b990148a726768d904a3367c02f5dac9f7
seem <mwlorgat@gmail.com>
2023-02-25 17:32:59 +0200

    Initial Commit

---
+++ TestNSStackView/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestNSStackView
+//
+//  Created by Wasim Lorgat on 2023/02/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
