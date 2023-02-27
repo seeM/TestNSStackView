a92f49ab15c1a23b3cb40d65a79bb852f16d2a40
seem <mwlorgat@gmail.com>
2023-02-27 16:28:02 +0200

    dont need delegate

--- TestNSStackView/ViewController.swift
+++ TestNSStackView/ViewController.swift
@@ -7,21 +7,7 @@
 
 import Cocoa
 
-class BorderedTextView: NSTextView, NSTextViewDelegate {
-    override init(frame frameRect: NSRect) {
-        super.init(frame: frameRect)
-    }
-    
-    override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
-        super.init(frame: frameRect, textContainer: container)
-        delegate = self
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+class BorderedTextView: NSTextView {
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
         let path = NSBezierPath(rect: bounds)
